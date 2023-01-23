fcfd33275cdc8564eb8c417221526cda5e0e7d1a
emagdnimMQT <mark.w.novak@gmail.com>
2023-01-23 10:48:13 -0500

    Created function to handle changing height display on screen resize.

--- height-dom-element/height-dom.js
+++ height-dom-element/height-dom.js
@@ -1,12 +1,21 @@
-const ctrOneHeight = document.querySelector('.ctr-one').offsetHeight;
+const ctrOne = document.querySelector('.ctr-one');
 const heightDisplay = document.querySelector(`[data-height]`);
+const changingHeightDisplay = document.querySelector(`[data-changing-height]`);
 const findBtn = document.querySelector('.find');
 const clearBtn = document.querySelector('.clear');
 
 findBtn.addEventListener('click', () => {
-    heightDisplay.innerText = ctrOneHeight
+    heightDisplay.innerText = ctrOne.offsetHeight;
 })
 
 clearBtn.addEventListener('click', () => {
     heightDisplay.innerText = '____'
 })
+
+function findHeight() {
+    changingHeightDisplay.innerText = ctrOne.offsetHeight;
+}
+
+findHeight();
+
+new ResizeObserver(findHeight).observe(ctrOne)
