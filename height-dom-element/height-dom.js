f9f6cc33150204d4d5df528ebc67c5a9f0bfc2cc
emagdnimMQT <mark.w.novak@gmail.com>
2023-01-25 14:42:32 -0500

    Added and refractored JS for the width change.

--- height-dom-element/height-dom.js
+++ height-dom-element/height-dom.js
@@ -1,21 +1,32 @@
 const ctrOne = document.querySelector('.ctr-one');
 const heightDisplay = document.querySelector(`[data-height]`);
+const widthtDisplay = document.querySelector(`[data-width]`);
 const changingHeightDisplay = document.querySelector(`[data-changing-height]`);
+const changingWidthDisplay = document.querySelector(`[data-changing-width]`);
 const findBtn = document.querySelector('.find');
 const clearBtn = document.querySelector('.clear');
 
 findBtn.addEventListener('click', () => {
     heightDisplay.innerText = ctrOne.offsetHeight;
+    widthtDisplay.innerText = ctrOne.offsetWidth;
 })
 
 clearBtn.addEventListener('click', () => {
-    heightDisplay.innerText = '____'
+    heightDisplay.innerText = '___';
+    widthtDisplay.innerText = '___';
 })
 
 function findHeight() {
     changingHeightDisplay.innerText = ctrOne.offsetHeight;
 }
-
 findHeight();
 
-new ResizeObserver(findHeight).observe(ctrOne)
+function findWidth() {
+    changingWidthDisplay.innerText = ctrOne.offsetWidth;
+}
+findWidth();
+
+new ResizeObserver(() => {
+    findHeight();
+    findWidth();
+}).observe(ctrOne)
