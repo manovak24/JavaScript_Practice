c2202bec564a3b360faebab28a53b395654bc8d6
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-02 08:59:41 -0500

    Getting closer

--- js-challenge.js
+++ js-challenge.js
@@ -1,19 +1,20 @@
 const arr = [0, 4, 6, 8, 8, 8, 5, 5, 7];
+let obj = {};
 
-const myFunction = (arr) => {
-    return arr.filter((currentValue, currentIndex) => 
-    arr.indexOf(currentValue) !== currentIndex)
+const myFunction = () => {
+    for (i = 0; i < arr.length; i++) {
+        if (obj[arr[i]]) {
+            obj[arr[i]] += 1;
+        } else {
+            obj[arr[i]] = 1;
+        }
+    }
+    for (let prop in obj) {
+        if (obj[prop] >= 2) {
+            console.log(prop + " counted " + obj[prop]) + " times.";
+        }
+    }
+    console.log(obj);
 }
 
-myFunction(arr);
-
-var array = [1, 4, 8, 2, 4, 1, 6, 2, 9, 7];
-function findDuplicates(arr) {
-    return arr.filter((currentValue, currentIndex) =>
-    arr.indexOf(currentValue) !== currentIndex);
-}
-
-findDuplicates(arr);
-
-
-console.log(arr)
+myFunction();
