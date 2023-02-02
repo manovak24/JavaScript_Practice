159e427323ded094cd0a9961e2a9bd512cae6faa
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-01 20:46:13 -0500

    First part complete.

--- js-challenge.js
+++ js-challenge.js
@@ -1,12 +1,17 @@
 const arr = [0, 4, 6, 8, 8, 8, 5, 5, 7];
 
-const myFunction = (ar) => {
-    for (i = 0; i <= ar.length; i++) {
-        // if(arr[i] === (arr[i+1])) {
-        //     console.log('this worked')
-        // }
-        console.log(arr[i+1])
-    }
+const myFunction = () => {
+    for (i = 0; i < arr.length; i++) {
+        // console.log(arr[i])
+        if(arr[i] === arr[i + 1] && arr[i + 1] === arr[i + 2]) {
+            const index = arr.indexOf(arr[i]);
+            // console.log(arr[i] + "arr i")
+            // console.log(index)
+            arr.splice(3, index);
+        }
+            
+    }    
+    console.log(arr)
 }
 
-myFunction(arr);
+myFunction();
