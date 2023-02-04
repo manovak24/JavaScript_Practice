05205f3a0e9eb236ebc12270a58ffea3748c2263
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-04 13:58:21 -0500

    Refractored problem 3 for best practice.

--- js-challenge.js
+++ js-challenge.js
@@ -10,17 +10,30 @@ function removeChar(str) {
 console.log(removeChar('eloquent'))
 
 
-function bmi(w, h) {
-    let hSq = h * h;
-    if(w/hSq <= 18.5) {
-        return "Underweight"
-    } else if (w/hSq <= 25.0) {
-        return "Normal"
-    } else if (w/hSq <= 30) {
-        return "Overweight"
-    } else if (w/hSq > 30) {
-        return "Obese"
+function bmi(weight, height) {
+    // let hSq = height * height;
+    // if(weight/hSq <= 18.5) {
+    //     return "Underweight"
+    // } else if (weight/hSq <= 25.0) {
+    //     return "Normal"
+    // } else if (weight/hSq <= 30) {
+    //     return "Overweight"
+    // } else if (weight/hSq > 30) {
+    //     return "Obese"
+    // }
+
+    var result = weight/Math.pow(height,2) 
+  
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25) {
+      return "Normal";
+    } else if (result <= 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
     }
+    
 }
 
 console.log(bmi(80, 1.80))
