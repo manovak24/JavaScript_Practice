3a0039245de661ec58f3b7db3fb392674a6dab57
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-04 12:22:56 -0500

    I feel so dumb.

--- js-challenge.js
+++ js-challenge.js
@@ -1,38 +1,28 @@
-const arr = [0, 4, 6, 8, 2, 9, 8, 4, 6];
-// [0, 4, 6, 8, 8, 8, 5, 5, 7];
-let obj = {};
-let answer;
+const arr = [0, 4, 6, 8, 8, 9, 8, 4, 6];
 
-const myFunction = () => {
-    // need to use something like a for when(?) loop for this to run as long as the arr.lenght is greater than 1
-    for (i = 0; i < arr.length; i++) {
-        if (obj[arr[i]]) {
-            obj[arr[i]] += 1;
-        } else {
-            obj[arr[i]] = 1;
-        }
-    }
-    for (let key in obj) {
-        if (obj[key] >= 2) {
-            // console.log(key + " counted " + obj[key]) + " times.";
-            let arrIndexMultiple = arr.indexOf(parseInt(key));
-            // console.log(arrIndex)
 
-            arr.splice(arrIndexMultiple, obj[key], obj[key] )
-        }
+const toFindDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index)
+const duplicateElements = toFindDuplicates(arr);
+console.log(duplicateElements);
 
-        if (obj[key] === 1) {
-            let arrIndexSingle = arr.indexOf(parseInt(key));
-            arr.splice(arrIndexSingle, obj[key], obj[key]);
-        }
-    }
-    console.log(arr)
+duplicateElements.forEach(dup => {
+   let index = arr.indexOf(dup)
+   console.log(index)
+})
 
-    if(arr.length === 1) {
-        answer = arr[0]
-        console.log(answer)
-    }
-    
-}
+console.log(duplicateElements);
 
-myFunction();
+console.log(arr.indexOf(4))
+
+// const myFunction = () => {
+//     for (i = 0; i < arr.length -1; i++) {
+//         let j = i + 1;
+//         if(arr[i] === arr[j]) {
+//             arr.splice(arr[i], 1, 1)
+//         }
+//         j++;
+//     }
+//     console.log(arr)
+// }
+
+// myFunction();
