a1e71b01af7af53f92c567574a4c718944eb7fd2
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-04 13:28:06 -0500

    Feeling humble.

--- js-challenge.js
+++ js-challenge.js
@@ -1,28 +1,5 @@
-const arr = [0, 4, 6, 8, 8, 9, 8, 4, 6];
+function numberToString(num) {
+    return num.toString()
+}
 
-
-const toFindDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index)
-const duplicateElements = toFindDuplicates(arr);
-console.log(duplicateElements);
-
-duplicateElements.forEach(dup => {
-   let index = arr.indexOf(dup)
-   console.log(index)
-})
-
-console.log(duplicateElements);
-
-console.log(arr.indexOf(4))
-
-// const myFunction = () => {
-//     for (i = 0; i < arr.length -1; i++) {
-//         let j = i + 1;
-//         if(arr[i] === arr[j]) {
-//             arr.splice(arr[i], 1, 1)
-//         }
-//         j++;
-//     }
-//     console.log(arr)
-// }
-
-// myFunction();
+console.log(numberToString(854))
