df18d63eb85050aa9645f7360ee7cca06a552354
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-13 10:04:03 -0500

    Worked on script for scroll animation.

---
+++ scroll-trigger-animation/scroll.js
@@ -0,0 +1,18 @@
+const reveal = () => {
+    const reveals = document.querySelectorAll('.text-box');
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        console.log(windowHeight)
+        const elementVisible = 50;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal);
+
+reveal()
