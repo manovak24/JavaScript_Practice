8a688a4b340d2c7936e1405155a62bdbea27cc9f
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-15 09:50:57 -0500

    Refractored code to make more efficient.

--- search-highligh-text/js-highlight-search-text.js
+++ search-highligh-text/js-highlight-search-text.js
@@ -4,58 +4,43 @@ const clearBtn = document.getElementById('clear-btn');
 const searchTerm = document.getElementById('text-to-search');
 const paragraph = document.querySelectorAll('.paragraph');
 
-// let paragraphObj = {
-//     // text: ''
-// };
-
 let paragraphArr = [];
 
 const search = (e) => {
     e.preventDefault();
-    // paragraphObj.text = paragraph.innerText;
-
-    paragraph.forEach(p => {
-        paragraphArr.push(p.innerText);
-    })
-
-    let textToSearch = searchTerm.value; 
-    textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+    let textToSearch = searchTerm.value.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
     let pattern = new RegExp(`${textToSearch}`, "gi");
-
     paragraph.forEach(p => {
+        // used to store original paragraph text to replace highlighted text when cleared
+        paragraphArr.push(p.innerText);
+        // replaces paragraph text with highlighted search term text
         p.innerHTML = p.textContent.replace(pattern, match => `<mark>${match}</mark>`);
-    })
-
+    });
     clearBtn.disabled = false;
-
-    // console.log(paragraphObj)
-    console.log(paragraphArr)
-}
+    // console.log(paragraphArr);
+    return;
+};
 
 const clearSearch = () => {
     for(let i = 0; i < paragraph.length; i++) {
         paragraph[i].innerText = paragraphArr[i]
     }
-
     paragraphArr = [];
     searchTerm.value = '';
-    // paragraph.innerText = paragraphObj.text;
     clearBtn.disabled = true;
-}
+    return;
+};
 
 submitCtr.addEventListener('submit', (event) => {
     search(event);
-})
+});
 
 submitBtn.addEventListener('click', (event) => {
     search(event);
-    // console.log(paragraphObj, "object");
-    // console.log(`THIS IS THE TEXT ${paragraphObj.text}`);
-
-})
+});
 
 clearBtn.addEventListener('click', () => {
     clearSearch();
-})
+});
 
 console.log(paragraph)
