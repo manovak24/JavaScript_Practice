edbca7f0bacce0694ca2b08b0495d4973442822f
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-16 09:17:30 -0500

    And I thought my solution was optimized...

--- js-challenge.js
+++ js-challenge.js
@@ -305,7 +305,14 @@ function setAlarm(employed, vacation) {
   // return employed && !vacation ? true : false;
   return employed && !vacation;
 }
-console.log(setAlarm(true, true));
-console.log(setAlarm(false, true));
-console.log(setAlarm(false, false));
-console.log(setAlarm(true, false));
+// console.log(setAlarm(true, true));
+// console.log(setAlarm(false, true));
+// console.log(setAlarm(false, false));
+// console.log(setAlarm(true, false));
+
+
+function openOrSenior(data) {
+  // return data.map(info => info[0] >= 55 && info[1] > 7 ? 'Senior' : 'Open');
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
