3470e4d1781329d7172b326e5e2da2718bf9e479
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-17 14:28:07 -0500

    And another practice problem.

--- js-challenge.js
+++ js-challenge.js
@@ -315,4 +315,11 @@ function openOrSenior(data) {
   // return data.map(info => info[0] >= 55 && info[1] > 7 ? 'Senior' : 'Open');
   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 }
-console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+// console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
+
+function DNAtoRNA(dna) {
+  // return dna.split('').map(item => item === 'T' ? 'U' : item).join('');
+  return dna.replace(/T/g, 'U');
+}
+
+console.log(DNAtoRNA('GCAT'))
