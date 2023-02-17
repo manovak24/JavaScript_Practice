d5881c887393d74e4d55cf5c597ea70b527fde1d
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-17 13:20:51 -0500

    Create js program to convert text.

---
+++ convert-text.js
@@ -0,0 +1,4 @@
+function convertText(str) {
+    return str.toUpperCase();
+}
+console.log(convertText('create space root down find balance inhale deeply cleanse the mind calm your body heal naturally'));
