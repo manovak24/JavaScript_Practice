f6079dc80c350383d6e6701f5a1d4617a014fb03
emagdnimMQT <mark.w.novak@gmail.com>
2023-02-28 21:59:35 -0500

    Stand still.

--- js-challenge.js
+++ js-challenge.js
@@ -438,4 +438,10 @@ const basicOp = (operation, value1, value2) => {
         return 0;
   }
 }
-console.log(basicOp('+', 4, 7));
+// console.log(basicOp('+', 4, 7));
+
+
+function points(games) {
+  return 
+}
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))
