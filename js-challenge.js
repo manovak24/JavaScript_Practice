98e4179ab6f1302967c5a3e7bd6b10b7c6aad14e
emagdnimMQT <mark.w.novak@gmail.com>
2023-03-28 09:28:37 -0400

    One slight tweak and got it.

--- js-challenge.js
+++ js-challenge.js
@@ -676,9 +676,12 @@ function abbrevName(name) {
 const wave = (str) => {
   let arr = [];
   for(let i = 0; i < str.length; i++) {
-    arr.push(str.slice(0, i) + str.charAt(i).toUpperCase() + str.slice(i+1));
+    if(str.charAt(i) !== ' '){
+      arr.push(str.slice(0, i) + str.charAt(i).toUpperCase() + str.slice(i+1));
+
+    }
   }
 
   return arr;
 }
-console.log(wave('hello'));
+console.log(wave('two words'));
