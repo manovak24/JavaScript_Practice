54ddb279a42a544a9b6bf711ab09950731906763
emagdnimMQT <mark.w.novak@gmail.com>
2023-04-05 07:49:25 -0400

    Working on response body object.

---
+++ object-test.js
@@ -0,0 +1,15 @@
+const data = {
+    "code": "CNw001",
+    "center_id": "4651f37e-735a-4299-a21d-ef2f322f420a",
+    "personal_info": {
+        "first_name": "abcd",
+        "last_name": "efgh",
+        "mobile_phone": {
+            "country_code": 225,
+            "number": "7777788888"
+        }
+    },
+    "address_info": {
+        "country_id": 95
+    }
+}
