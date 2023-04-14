f6b77ca411b4969a42a4bdcce68cd63f9f0c96ed
emagdnimMQT <mark.w.novak@gmail.com>
2023-04-14 15:49:51 -0400

    That was dumb.

--- js-challenge.js
+++ js-challenge.js
@@ -897,4 +897,20 @@ function checkForBase(base, factor) {
 function shortcut (string) {
   return string.replace(/[aeiou]/gi, '');
 }
-console.log(shortcut('test'));
+// console.log(shortcut('test'));
+
+
+function seriesSum(n) {
+  let result = 0;
+  let reverage = 1;
+  for (let i = 0; i < n; i += 1) {
+    if (i === 0) {
+      result = 1;
+    } else {
+      reverage += 3;
+      result = result + (1 / reverage);
+    }
+  }
+  return result.toFixed(2);
+};
+console.log(seriesSum(4))
