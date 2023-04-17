74c3830054ca8a4a8244bf183017cd77c640f57d
emagdnimMQT <mark.w.novak@gmail.com>
2023-04-17 09:57:25 -0400

    Commenting out console log

--- js-challenge.js
+++ js-challenge.js
@@ -948,4 +948,4 @@ function expandedForm(num) {
 
   return num.toString().split("").reverse().map( (a, i) => a * Math.pow(10, i)).filter(a => a > 0).reverse().join(" + ");
 }
-console.log(expandedForm(420370022));
+// console.log(expandedForm(420370022));
