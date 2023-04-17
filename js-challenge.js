ad5d0f252d9d5ae6429b17d4a857fa09279e80d9
emagdnimMQT <mark.w.novak@gmail.com>
2023-04-17 09:56:49 -0400

    Optimized is so cleannnnnn

--- js-challenge.js
+++ js-challenge.js
@@ -935,15 +935,17 @@ function updateLight(current) {
 
 
 function expandedForm(num) {
-  let arr = num.toString().split('');
-  let count = [];
-  for(let i = num.toString().length; i > 0; i--) {
-      count.push(i - 1)
-  }
-  let newArr = [];
-  for(let i = 0; i < arr.length; i++){
-    newArr.push(arr[i].padEnd(count[i] + 1, '0'))
-  }
-  return newArr.map(item => parseInt(item)).filter(item => item > 0).map(item => item.toString()).join(' + ');
+  // let arr = num.toString().split('');
+  // let count = [];
+  // for(let i = num.toString().length; i > 0; i--) {
+  //     count.push(i - 1)
+  // }
+  // let newArr = [];
+  // for(let i = 0; i < arr.length; i++){
+  //   newArr.push(arr[i].padEnd(count[i] + 1, '0'))
+  // }
+  // return newArr.map(item => parseInt(item)).filter(item => item > 0).map(item => item.toString()).join(' + ');
+
+  return num.toString().split("").reverse().map( (a, i) => a * Math.pow(10, i)).filter(a => a > 0).reverse().join(" + ");
 }
 console.log(expandedForm(420370022));
