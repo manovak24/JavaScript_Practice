f32c3c0a6190fc8c7f75fbfda36686fe0a1a93cf
emagdnimMQT <mark.w.novak@gmail.com>
2023-04-26 08:39:02 -0400

    Commented out the console.log

--- js-challenge.js
+++ js-challenge.js
@@ -1104,4 +1104,4 @@ const digitize = (n) => {
 const order = (words) => {
   return words.split(' ').sort((a,b) => a.match(/\d+/g) - b.match(/\d+/g)).join(' ');
 }
-console.log(order('is2 Thi1s T4est 3a'));
+// console.log(order('is2 Thi1s T4est 3a'));
