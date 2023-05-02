8f42712aebf3380478c30e24c38c328ac7d3b22d
emagdnimMQT <mark.w.novak@gmail.com>
2023-05-02 11:43:14 -0400

    Finished jQuery for menu, updated sytles, and modified html.

--- sticky-jQuery-hamburger-menu/main.js
+++ sticky-jQuery-hamburger-menu/main.js
@@ -1,15 +1,17 @@
-$(".cross").hide();
 $(".menu").hide();
 $("#nav-icon").click(function () {
-    $(".menu").slideToggle("slow");
-});
 
-$(".cross").click(function () {
-    $(".menu").slideToggle("slow");
-});
-
-$(document).ready(function () {
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon').click(function () {
-        $(this).toggleClass('open');
-    });
+    if ($(this).hasClass("open")) {
+        $(".menu").slideUp(200, function() {
+            $(".custom-nav-header").css('border-bottom-left-radius', '13.44px');
+            $(".custom-nav-header").css('border-bottom-right-radius', '13.44px');
+        });
+        $(this).removeClass("open");
+    } else {
+        $(".custom-nav-header").css('border-bottom-left-radius', '0');
+        $(".custom-nav-header").css('border-bottom-right-radius', '0');
+        $(".menu").slideDown(200);
+        $(this).addClass("open")
+        
+    }
 });
