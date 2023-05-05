a96e68fe86869faa07091163d3b0e2b239e5a353
emagdnimMQT <mark.w.novak@gmail.com>
2023-05-05 08:55:16 -0400

    Nice and easy.

--- js-challenge.js
+++ js-challenge.js
@@ -1154,3 +1154,10 @@ function noSpace(x) {
   return x.replace(/\s/g, '');
 }
 // console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
+
+function mouthSize(animal) {
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+}
+// console.log(mouthSize('ALLIGATOR'));
+// console.log(mouthSize('toucan'));
