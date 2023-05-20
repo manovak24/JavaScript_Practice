fd44adafabbb5ac21a05160677088e151dc2f7f2
emagdnimMQT <mark.w.novak@gmail.com>
2023-05-20 10:05:14 -0400

    I love javascript.

--- js-challenge.js
+++ js-challenge.js
@@ -1258,6 +1258,27 @@ function simpleMutipleication(number) {
 const oddOrEven = (array) => {
   return array.reduce((a,b) => a + b, 0) % 2 ? 'odd' : 'even';
 }
-console.log(oddOrEven([2]));
-console.log(oddOrEven([1]));
-console.log(oddOrEven([]));
+// console.log(oddOrEven([2]));
+// console.log(oddOrEven([1]));
+// console.log(oddOrEven([]));
+
+const lovefunc = (flower1, flower2) => {
+  return flower1 % 2 !== flower2 % 2;
+}
+// console.log(lovefunc(2, 4));
+
+
+function finalGrade(exam, projects) {
+  if(exam > 90 || projects > 10) {
+    return 100;
+  } else if(exam > 75 && projects >= 5) {
+    return 90;
+  } else if(exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+// console.log(finalGrade(100, 12));
+// console.log(finalGrade(99, 0));
+// console.log(finalGrade(85, 5));
