c6c3d4f33cc4544acd862fdec76366b584c7becc
emagdnimMQT <mark.w.novak@gmail.com>
2023-06-07 12:52:59 -0400

    One more.

--- js-challenge.js
+++ js-challenge.js
@@ -1487,4 +1487,10 @@ function solve(s) {
 const replace = (s) => {
   return s.replace(/[aeiou]/ig, '!');
 }
-console.log(replace('Hi!'));
+// console.log(replace('Hi!'));
+
+
+const mergeArrays = (arr1, arr2) => {
+  return [...new Set([...arr1, ...arr2])].sort((a,b) => a - b);
+}
+console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]))
