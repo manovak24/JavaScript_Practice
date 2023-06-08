279936ff9a1248b0c259759ce953150a98df015c
emagdnimMQT <mark.w.novak@gmail.com>
2023-06-08 09:34:56 -0400

    Getting warmed up.

--- js-challenge.js
+++ js-challenge.js
@@ -1493,4 +1493,10 @@ const replace = (s) => {
 const mergeArrays = (arr1, arr2) => {
   return [...new Set([...arr1, ...arr2])].sort((a,b) => a - b);
 }
-console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]))
+// console.log(mergeArrays([1,3,5,7,9], [10,8,6,4,2]));
+
+
+const reverseList = (list) => {
+  return list.reverse();
+}
+// console.log(reverseList([1,2,3,4]));
