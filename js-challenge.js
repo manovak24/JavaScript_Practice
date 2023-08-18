c41ba39ab66ce259fa72c86e0359723175f5ae4b
emagdnimMQT <mark.w.novak@gmail.com>
2023-08-18 08:19:10 -0400

    Fun one while waiting.

--- js-challenge.js
+++ js-challenge.js
@@ -2213,4 +2213,24 @@ function position(letter) {
 const toBinary = (n) => {
   return parseInt(n.toString(2));
 }
-console.log(toBinary(1));
+// console.log(toBinary(1));
+
+
+const humanYearsCatYearsDogYears = function(humanYears) {
+  let dogYears = 0;
+  let catYears = 0;
+  for(let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      dogYears += 15;
+      catYears += 15;
+    } else if (i === 2) {
+      dogYears += 9;
+      catYears += 9;
+    } else if (i > 2) {
+      dogYears += 5;
+      catYears += 4;
+    }
+  }
+  return [humanYears, catYears, dogYears];
+}
+// console.log(humanYearsCatYearsDogYears(10));
