02e9de9f817f03548a2779541dec7f63d8391e90
emagdnimMQT <mark.w.novak@gmail.com>
2023-09-30 11:32:04 -0400

    Another one.

--- js-challenge.js
+++ js-challenge.js
@@ -2592,4 +2592,20 @@ const towerBuilder = (nFloors) => {
   }
   return building;
 }
-console.log(towerBuilder(5));
+// console.log(towerBuilder(5));
+
+
+const whatDay = (num) => {
+  const dayOfWeek = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday'
+  };
+
+  return dayOfWeek[num] || 'Wrong, please enter a number between 1 and 7';
+}
+console.log(whatDay(1));
