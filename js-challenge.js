1f0331b102f8f0b1b20e5ecabe10e9ac251b88df
emagdnimMQT <mark.w.novak@gmail.com>
2023-10-21 09:53:14 -0400

    Two ways to solve this one.

--- js-challenge.js
+++ js-challenge.js
@@ -2746,3 +2746,11 @@ function productArray(numbers) {
   return numbers.map(x => numbers.reduce((a,b) => a * b) / x)
 }
 // console.log(productArray([4, 3, 10, 3, 93, 79]));
+
+
+function maxTriSum(numbers) {
+  // return numbers.filter((num, i) => numbers.indexOf(num) === i).sort((a,b) => b - a).slice(0, 3).reduce((a,b) => a + b);
+
+  return ([...new Set(numbers)]).sort((a,b) => a-b).slice(-3).reduce((a,b) => a + b);
+}
+console.log(maxTriSum([3,2,6,8,2,3]));
