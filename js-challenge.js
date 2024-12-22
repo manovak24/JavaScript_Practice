139d8bc761a784b77e31c9878776f951a907ddc0
emagdnimMQT <mark.w.novak@gmail.com>
2024-12-22 08:46:44 -0500

    Fun warmup.

--- js-challenge.js
+++ js-challenge.js
@@ -5660,6 +5660,18 @@ function sumDupe () {
 // console.log(sumDupe(1, 2, 3));
 
 
+// const list1 = [
+//   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+//   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+// ]
+const findAdmin = (list, lang) => {
+  return list.filter(obj => obj.language === lang && obj.githubAdmin === 'yes');
+}
+// console.log(findAdmin(list1, 'JavaScript'));
+
+
 
 
 
