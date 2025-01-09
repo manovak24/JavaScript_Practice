f4ad5f15bf8458085ecd85a9c35dd414f7fa61a6
emagdnimMQT <mark.w.novak@gmail.com>
2025-01-09 09:13:18 -0500

    Warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -5725,7 +5725,7 @@ const findAll = (array, n) => {
 const catchSignChange = (arr) => {
   let count = 0;
   for(let i = 0; i < arr.length; i++) {
-    if(arr[i] < 0 && arr[i + 1] > 0 || arr[i] > 0 && arr[i + 1] < 0) {
+    if(arr[i] < 0 && arr[i + 1] >= 0 || arr[i] >= 0 && arr[i + 1] < 0) {
       count++;
     }
   }
