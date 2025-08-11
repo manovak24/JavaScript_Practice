f5f914bcc7ce763dd319ab462309922f259776de
emagdnimMQT <mark.w.novak@gmail.com>
2025-08-11 10:15:16 -0400

    Warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -6992,16 +6992,33 @@ const orderFood = (list) => {
   })
   return foodOptions;
 }
-console.log(orderFood([
-  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
-    meal: 'vegetarian' },
-  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
-    meal: 'standard' },
-  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
-    meal: 'vegan' },
-  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
-    meal: 'vegetarian' },
-]));
+// console.log(orderFood([
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+//     meal: 'vegetarian' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+//     meal: 'standard' },
+//   { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+//     meal: 'vegan' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+//     meal: 'vegetarian' },
+// ]));
+
+  function add(x) {
+	  return x + 10;	
+  }
+
+  function mult(x) {
+    return x * 30;
+  }
+
+function chain(input, fs) {
+  let result = input;
+  for(let i = 0; i < fs.length; i++) {
+    result = fs[i](result);
+  }
+  return result;
+}
+console.log(chain(2, [add, mult]));
 
 
 
