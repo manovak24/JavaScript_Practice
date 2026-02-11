bd890b9ab21a6fc099de82e0237e2bb7885d6150
emagdnimMQT <mark.w.novak@gmail.com>
2026-02-11 08:48:59 -0500

    Added comment.

--- js-challenge.js
+++ js-challenge.js
@@ -8509,7 +8509,7 @@ const solveSubstringCount = (s) => {
 // console.log(solveSubstringCount('43783226132897277463854229928459879476742846412'));
 
 
-// https://www.codewars.com/kata/6965d769930fb2eff921668f/train/javascript
+// Recursion example
 const solutionFibonacci = (first, second) => {
   let arr = [second, first];
   const backwards = (a, b) => {
