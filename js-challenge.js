6d8ee4f8ab5dc6e5bc47a3d8514193f0ae9b10e0
emagdnimMQT <mark.w.novak@gmail.com>
2026-02-17 09:00:37 -0500

    Another warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -8564,7 +8564,29 @@ const sumSquaresTwo = (array) => {
 const isUndefined = (value) => {
   return value === undefined
 }
-console.log(isUndefined());
+// console.log(isUndefined());
+
+
+const employees = [
+  { firstName: 'Ollie', lastName: 'Hepburn', role: 'Boss' },
+  { firstName: 'Morty', lastName: 'Smith', role: 'Truck Driver' },
+  { firstName: 'Peter', lastName: 'Ross', role: 'Warehouse Manager' },
+  { firstName: 'Cal', lastName: 'Neil', role: 'Sales Assistant' },
+  { firstName: 'Jesse', lastName: 'Saunders', role: 'Admin' },
+  { firstName: 'Anna', lastName: 'Jones', role: 'Sales Assistant' },
+  { firstName: 'Carmel', lastName: 'Hamm', role: 'Admin' },
+  { firstName: 'Tori', lastName: 'Sparks', role: 'Sales Manager' },
+  { firstName: 'Peter', lastName: 'Jones', role: 'Warehouse Picker' },
+  { firstName: 'Mort', lastName: 'Smith', role: 'Warehouse Picker' },
+  { firstName: 'Anna', lastName: 'Bell', role: 'Admin' },
+  { firstName: 'Jewel', lastName: 'Bell', role: 'Receptionist' },
+  { firstName: 'Colin', lastName: 'Brown', role: 'Trainee' }
+]
+const findEmployeesRole = (name) => {
+  let employee = employees.filter(x => x.firstName + ' ' + x.lastName === name)[0];
+  return employee ? employee.role : 'Does not work here!';
+}
+// console.log(findEmployeesRole('Morty Smith'));
 
 
 
