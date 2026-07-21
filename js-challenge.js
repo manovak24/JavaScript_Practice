c4d7310b7e392d5cf55ffe213a233b2760781392
emagdnimMQT <mark.w.novak@gmail.com>
2026-07-21 09:28:50 -0400

    Warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -9840,15 +9840,13 @@ const strayNumber = (numbers) => {
 
 
 const createPhoneNumber = (numbers) => {
-  
-  numbers.splice(0, 0, '(')
-  numbers.splice(4, 0, ')')
-  numbers.splice(5, 0, ' ')
-  numbers.splice(9, 0, '-')
-
-  return numbers.join('');
+  let format = '(xxx) xxx-xxxx';
+  for(let i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i]);
+  }
+  return format;
 }
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 
 // Fizzbuzz problems!!
