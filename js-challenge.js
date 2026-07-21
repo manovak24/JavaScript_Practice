9cfda4d02f20126b82341d06f3d09e9a6c5338c4
emagdnimMQT <mark.w.novak@gmail.com>
2026-07-21 09:26:55 -0400

    Warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -9836,7 +9836,19 @@ const strayNumber = (numbers) => {
 
   return Number(Object.keys(obj).find(key => obj[key] === 1));
 }
-console.log(strayNumber([17, 17, 3, 17, 17, 17, 17]));
+// console.log(strayNumber([17, 17, 3, 17, 17, 17, 17]));
+
+
+const createPhoneNumber = (numbers) => {
+  
+  numbers.splice(0, 0, '(')
+  numbers.splice(4, 0, ')')
+  numbers.splice(5, 0, ' ')
+  numbers.splice(9, 0, '-')
+
+  return numbers.join('');
+}
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 
 // Fizzbuzz problems!!
