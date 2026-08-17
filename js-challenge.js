5f9636f1853f57fac64237317b9a3ad2a9510094
emagdnimMQT <mark.w.novak@gmail.com>
2026-08-17 11:09:10 -0400

    Warm up.

--- js-challenge.js
+++ js-challenge.js
@@ -9973,6 +9973,12 @@ const sortBasedOnArray = (initialArray, sortingArray) => {
 // console.log(sortBasedOnArray(['x', 'y', 'z'], [1, 2, 0]));
 
 
+const squareOrSquareRoot = (array) => {
+  return array.map(num => Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num) : Math.pow(num, 2));
+}
+console.log(squareOrSquareRoot([4,3,9,7,2,1]));
+
+
 
 // Fizzbuzz problems!!
 
